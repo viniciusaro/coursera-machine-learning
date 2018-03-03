93a213dbf2a0353e74a3917b0f1c6a0965a7c929
Vinicius Rodrigues <vinicius.a.ro@gmail.com>
2018-03-02 21:55:12 -0300

    Add cost function vectorized implementation

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -8,8 +8,7 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+temp_grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -20,12 +19,30 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% ====================== FIRST VERSION ======================
+% cost = 0;
+% for i = 1:size(y),
+% 	cost = cost + (-y(i)*log(sigmoid(X(i, :) * theta)) - (1 - y(i))*(log(1 - sigmoid(X(i, :) * theta))));
+% end
 
+% for j = 1:size(theta),
+% 	partial_grad = 0;
+% 	for i = 1:size(y),
+% 		partial_grad = partial_grad + ((sigmoid(X(i, :) * theta) - y(i)) * X(i, :));
+% 	end
+% 	temp_grad(j) = sum(partial_grad)/m;
+% end
 
+% ====================== INNER FOR VECTORIZED ======================
 
+% for j = 1:size(theta),
+% 	temp_grad(j) = sum(sum((sigmoid(X * theta) - y) .* X/m));
+% end
 
+% ====================== FINAL VERSION :D ======================
 
-
+J = (-y'*log(sigmoid(X * theta)) - (1 - y')*log(1 - sigmoid(X * theta)))/m;
+grad = sum((sigmoid(X * theta) - y) .* X/m) * theta;
 
 % =============================================================
 
